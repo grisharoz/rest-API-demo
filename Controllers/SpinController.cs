c3c52128fef9371a5b340b37c325d227d341ed8f
Dmitry Mikhailyuk <grinpigrin@gmail.com>
2025-02-14 15:58:06 +0100

    spin

--- Controllers/SpinController.cs
+++ Controllers/SpinController.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
-using System.Diagnostics;
 
 namespace TodoApi.Controllers;
 
@@ -103,255 +99,192 @@ public class SpinController : ControllerBase
     [HttpPut("spin")]
     // public async Task<IActionResult> PutUser(long id, int bet)
     // bet-тип ставки(чётное/нечётное); input=чётное; bet=20$;
-    public ActionResult<Balance> Spin(long id, [FromForm] BetDTO bet)
+    public async Task<IActionResult> PutUser(long id, [FromBody] List<BetDTO> bets)
     {
+
+        if (bets == null || !bets.Any())
+        {
+            return BadRequest("The request body must contain at least one bet.");
+        }
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
 
-        string[] options = { "number", "color", "rank", "row", "numberSet", "column", "sequence", "even" };
-
-        List<Bet> bets = new List<Bet>();
-
-        System.Diagnostics.Debug.WriteLine("options", options);
-
-        if (Array.IndexOf(options, bet) == -1)
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
         {
-            return BadRequest("This rate is not on the list. Try again");
+            return NotFound();
         }
 
-        return Ok(new { message = "Data updated successfully!" });
-         
-        // var user = await _context.Users.FindAsync(id);
-        // if (user == null)
-        // {
-        //     return NotFound();
-        // }
-       
+        foreach (BetDTO bet in bets)
+        {
+            if (bet.Value.Length == 0)
+            {
+                return BadRequest("Value should be longer than 0");
+            }
+            if (bet.Stake == 0)
+            {
+                return BadRequest("Stake should be longer than 0");
+            }
 
-        // foreach (Bet right in bets)
-        // {
-        //     if (bet.type.Length == 0 || bet.value.Length == 0 || bet.stake == 0)
-        //     {
-        //         return BadRequest("Type or/and value and/or stake should be longer than 0");
-        //     }
-        //     if (bet.type == "number")
-        //     {
-        //         List<string> validChoices = Enumerable.Range(0, 37).Select(i => i.ToString()).ToList();
+            if (bet.Type == "number")
+            {
+                List<string> validChoices = Enumerable.Range(0, 37).Select(i => i.ToString()).ToList();
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Numbers should be from 0 to 36.");
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
-        //         }
-        //     }
-        //     if (bet.type == "color")
-        //     {
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Numbers should be from 0 to 36.");
+                }
+            }
+            if (bet.Type == "color")
+            {
 
-        //         List<string> validChoices = new List<string>() { "red", "black", "green" };
+                List<string> validChoices = new List<string>() { "red", "black", "green" };
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Color should be red, black or green");
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Color should be red, black or green");
 
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
+                }
+            }
+            if (bet.Type == "even")
+            {
+                List<string> validChoices = ["even", "not even"];
 
-        //         }
-        //     }
-        //     if (bet.type == "even")
-        //     {
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Even should be even or not even.");
 
-        //         List<string> validChoices = ["even", "not even"];
+                }
+            }
+            if (bet.Type == "row")
+            {
 
+                List<string> validChoices = Enumerable.Range(1, 3).Select(i => i.ToString()).ToList();
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Even should be even or not even.");
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Row should be from 1 to 3.");
 
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
+                }
+            }
+            if (bet.Type == "column")
+            {
 
-        //         }
-        //     }
-        //     if (bet.type == "row")
-        //     {
+                List<string> validChoices = Enumerable.Range(1, 12).Select(i => i.ToString()).ToList();
 
-        //         List<string> validChoices = Enumerable.Range(1, 3).Select(i => i.ToString()).ToList();
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Column should be from 1 to 12.");
+                }
+            }
+            if (bet.Type == "rank")
+            {
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Row should be from 1 to 3.");
+                List<string> validChoices = ["1/18", "19/36"];
 
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Rank should be 1/18 or 19/36");
 
-        //         }
-        //     }
-        //     if (bet.type == "column")
-        //     {
+                }
+            }
+            if (bet.Type == "sequence")
+            {
 
-        //         List<string> validChoices = Enumerable.Range(1, 12).Select(i => i.ToString()).ToList();
+                List<string> validChoices = ["1/12", "13/24", "25/36"];
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Column should be from 1 to 12.");
+                if (!validChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Sequency should be 1/12 or 13/24 or 25/36");
+                }
+            }
+            if (bet.Type == "numberSet")
+            {
+                string[] validCollectionChoices = ["1/2", "1/4", "1/2/4/5", "1/2/3/4/5/6", "2/3", "2/5", "2/3/5/6", "3/6", "4/5", "5/6", "5/8", "4/5/7/8", "5/6/8/9", "4/5/6/7/8/9", "6/9", "4/7", "7/8", "7/10", "7/8/10/11", "7/8/9/10/11/12", "8/9", "8/11", "8/9/11/12", "9/12", "10/11", "10/13", "10/11/13/14", "10/11/12/13/14/15", "11/12", "11/14", "11/12/14/15", "12/15", "13/14", "13/16", "13/14/16/17", "13/14/15/16/17/18", "14/15", "15/18", "14/15/17/18", "16/17", "16/19", "16/17/19/20", "16/17/18/19/20/21", "14/17", "17/18", "17/20", "17/18/20/21", "18/21", "19/20", "19/22", "19/20/22/23", "19/20/21/22/23/24", "20/21", "20/23", "20/21/23/24", "21/24", "22/23", "22/25", "22/23/25/26", "22/23/24/25/26", "23/24", "23/26", "23/24/26/27", "22/23/24/25/26/27", "24/27", "25/26", "25/28", "25/26/28/29", "25/26/27/28/29/30", "26/27", "26/29", "26/27/29/30", "27/30", "28/29", "28/31", "28/29/31/32", "28/29/30/31/32/33", "29/30", "29/32", "29/30/32/33", "30/33", "31/32", "31/34", "31/32/34/35", "31/32/33/34/35/36", "32/33", "32/35", "32/33/35/36", "33/36", "34/35", "35/36"];
 
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
+                if (!validCollectionChoices.Contains(bet.Value))
+                {
+                    return BadRequest("Invalid range. Not found this numberset");
+                }
+            }
+        }
 
-        //         }
-        //     }
-        //     if (bet.type == "rank")
-        //     {
+        Spin(bets, user);
 
-        //         List<string> validChoices = ["1/18", "19/36"];
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!UsersController.UserExists(id, _context))
+        {
+            return NotFound();
+        }
 
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Rank should be 1/18 or 19/36");
-
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
-
-        //         }
-        //     }
-        //     if (bet.type == "sequence")
-        //     {
-
-        //         List<string> validChoices = ["1/12", "13/24", "25/36"];
-
-        //         if (!validChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Sequency should be 1/12 or 13/24 or 25/36");
-
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
-
-        //         }
-        //     }
-        //     if (bet.type == "numberSet")
-        //     {
-
-        //         string[] validCollectionChoices = ["1/2", "1/4", "1/2/4/5", "1/2/3/4/5/6", "2/3", "2/5", "2/3/5/6", "3/6", "4/5", "5/6", "5/8", "4/5/7/8", "5/6/8/9", "4/5/6/7/8/9", "6/9", "4/7", "7/8", "7/10", "7/8/10/11", "7/8/9/10/11/12", "8/9", "8/11", "8/9/11/12", "9/12", "10/11", "10/13", "10/11/13/14", "10/11/12/13/14/15", "11/12", "11/14", "11/12/14/15", "12/15", "13/14", "13/16", "13/14/16/17", "13/14/15/16/17/18", "14/15", "15/18", "14/15/17/18", "16/17", "16/19", "16/17/19/20", "16/17/18/19/20/21", "14/17", "17/18", "17/20", "17/18/20/21", "18/21", "19/20", "19/22", "19/20/22/23", "19/20/21/22/23/24", "20/21", "20/23", "20/21/23/24", "21/24", "22/23", "22/25", "22/23/25/26", "22/23/24/25/26", "23/24", "23/26", "23/24/26/27", "22/23/24/25/26/27", "24/27", "25/26", "25/28", "25/26/28/29", "25/26/27/28/29/30", "26/27", "26/29", "26/27/29/30", "27/30", "28/29", "28/31", "28/29/31/32", "28/29/30/31/32/33", "29/30", "29/32", "29/30/32/33", "30/33", "31/32", "31/34", "31/32/34/35", "31/32/33/34/35/36", "32/33", "32/35", "32/33/35/36", "33/36", "34/35", "35/36"];
-
-
-        //         if (!validCollectionChoices.Contains(bet.value))
-        //         {
-        //             return BadRequest("Invalid range. Not found this numberset");
-
-        //         }
-        //         else
-        //         {
-        //             bets.Add(new Bet(bet.type, bet.value, bet.stake));
-
-        //         }
-        //     }
-
-        // }
-
-
-        // bets.Add(new Bet("number", "2", 20));
-        // bets.Add(new Bet("color", "red", 30)); 
-        // bets.Add(new Bet("even", "true", 30));
-        // bets.Add(new Bet("row", "1", 30));
-        // bets.Add(new Bet("column", "1", 30));
-        // bets.Add(new Bet("rank", "18/36", 30));
-        // bets.Add(new Bet("sequence", "25/36", 30));
-        // bets.Add(new Bet("numberSet", "34/35/36", 30));
-
-        // Spin(bets, user.Balance);
-
-        // user.Balance = 305;
-        // Balance balance = new Balance(user.Balance, true, 200);
-
-        // try
-        // {
-        //     await _context.SaveChangesAsync();
-        // }
-        // catch (DbUpdateConcurrencyException) when (!UsersController.UserExists(id, _context))
-        // {
-        //     return NotFound();
-        // }
-
-        // return balance;
+        return Ok(new { message = "Пользователь успешно обновлён!" });
 
     }
 
-    // public static void Spin(List<Bet> bets, int balance)
-    // {
+    public static void Spin(List<BetDTO> bets, User user)
+    {
 
-    //     var roulette = new ListOfRoulette();
-    //     Random random = new Random();
-    //     string rand = random.Next(roulette.Numbers.Count()).ToString();
+        var roulette = new ListOfRoulette();
+        Random random = new Random();
+        string rand = random.Next(roulette.Numbers.Count()).ToString();
 
-    //     Dictionary<string, int> coefficients = new Dictionary<string, int>(){
-    //         {"2",18},
-    //         {"3",12},
-    //         {"4",9},
-    //         {"6",6},
-    //     };
+        Dictionary<string, int> coefficients = new Dictionary<string, int>(){
+            {"2",18},
+            {"3",12},
+            {"4",9},
+            {"6",6},
+        };
 
-    //     Console.WriteLine($"На рулетке выпало: {rand}");
+        Console.WriteLine($"На рулетке выпало: {rand}");
 
-    //     bets.ForEach(delegate (Bet bet)
-    //     {
-    //         string type = bet.type;
-    //         string value = bet.value;
-    //         int stakeAmount = bet.stake;
+        bets.ForEach(delegate (BetDTO bet)
+        {
+            string type = bet.Type;
+            string value = bet.Value;
+            int stakeAmount = bet.Stake;
 
-    //         if (type == "numberSet")
-    //         {
-    //             if (roulette.getNumberByKey(rand).getByAttribute(type) is List<string> collectionOfNumbers)
-    //             {
+            if (type == "numberSet")
+            {
+                if (roulette.getNumberByKey(rand).getByAttribute(type) is List<string> collectionOfNumbers)
+                {
 
-    //                 if (collectionOfNumbers.Contains(value))
-    //                 {
-    //                     string collectionOfNumbersCount = value.Split("/").Length.ToString();
+                    if (collectionOfNumbers.Contains(value))
+                    {
+                        string collectionOfNumbersCount = value.Split("/").Length.ToString();
 
-    //                     balance += stakeAmount * coefficients[collectionOfNumbersCount];
-    //                     Console.WriteLine($"Поздравляю! Ты выиграл {stakeAmount * coefficients[collectionOfNumbersCount]} долларов, поставив на {value}");
-    //                 }
-    //                 else
-    //                 {
-    //                     Console.WriteLine($"К сожалению, ты проиграл {stakeAmount} долларов, поставив на {value}");
-    //                 }
-    //             }
-    //         }
-    //         else
-    //         {
-    //             if (roulette.getNumberByKey(rand).getByAttribute(type).ToString() == value)
-    //             {
+                        user.Balance += stakeAmount * coefficients[collectionOfNumbersCount];
+                        Console.WriteLine($"Поздравляю! Ты выиграл {stakeAmount * coefficients[collectionOfNumbersCount]} долларов, поставив на {value}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"К сожалению, ты проиграл {stakeAmount} долларов, поставив на {value}");
+                    }
+                }
+            }
+            else
+            {
+                if (roulette.getNumberByKey(rand).getByAttribute(type).ToString() == value)
+                {
 
-    //                 balance += stakeAmount * getCoefficient(type);
-    //                 Console.WriteLine($"Поздравляю! Ты выиграл {stakeAmount * getCoefficient(type)} долларов, поставив на {value}");
-    //                 Console.WriteLine($"Твой баланс: {balance}$");
-    //             }
-    //             else
-    //             {
-    //                 balance -= stakeAmount;
-    //                 Console.WriteLine($"К сожалению, ты проиграл {stakeAmount} долларов, поставив на {value}");
-    //                 Console.WriteLine($"Твой баланс: {balance}$");
+                    user.Balance += stakeAmount * getCoefficient(type);
+                    Console.WriteLine($"Поздравляю! Ты выиграл {stakeAmount * getCoefficient(type)} долларов, поставив на {value}");
+                    Console.WriteLine($"Твой баланс: {user.Balance}$");
+                }
+                else
+                {
+                    user.Balance -= stakeAmount;
+                    Console.WriteLine($"К сожалению, ты проиграл {stakeAmount} долларов, поставив на {value}");
+                    Console.WriteLine($"Твой баланс: {user.Balance}$");
+                }
+            }
 
-    //             }
-    //         }
+        });
 
-    //     });
-    // }
+
+    }
 }
--- Models/Bet.cs
+++ Models/Bet.cs
@@ -1,4 +1,32 @@
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Linq;
+
+public class AllowedValuesAttribute : ValidationAttribute
+{
+    private readonly string[] _allowedValues;
+
+    public AllowedValuesAttribute(params string[] allowedValues)
+    {
+        _allowedValues = allowedValues;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        // Если значение отсутствует, пусть [Required] обработает это отдельно
+        if (value == null)
+            return ValidationResult.Success;
+
+        if (value is string stringValue)
+        {
+            if (_allowedValues.Contains(stringValue))
+                return ValidationResult.Success;
+        }
+        
+        var allowed = string.Join(", ", _allowedValues);
+        return new ValidationResult(ErrorMessage ?? $"Значение должно быть одним из следующих: {allowed}.");
+    }
+}
 
 public class Bet
 {
@@ -11,6 +39,8 @@ public class Bet
 public class BetDTO
 {
     [Required]
+    [AllowedValues("number", "color", "rank", "row", "numberSet", "column", "sequence", "even",
+        ErrorMessage = "This type is incorect. Possible types: number, color, rank, row, numberSet, column, sequence, even")]
     public string Type { get; set; }
     [Required]
     public string Value { get; set; }
